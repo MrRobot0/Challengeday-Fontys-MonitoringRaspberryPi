f4d8194a9647e02b830807094f7ba87721a3f73d
Wietze van Osch <wietze@osch.net>
2019-03-29 10:49:54 +0100

    Update

--- app.py
+++ app.py
@@ -3,10 +3,66 @@ import datetime
 import socket
 import shutil
 import psutil
+import struct
+import sys
+import time
 app = Flask(__name__, static_url_path='/static')
+
+
+
+
+
+
 
 @app.route("/simpel")
 def simple():
+
+	def synctime():
+		# List of servers in order of attempt of fetching
+		server_list = ['ntp.iitb.ac.in', 'time.nist.gov', 'time.windows.com', 'pool.ntp.org']
+
+		'''
+		Returns the epoch time fetched from the NTP server passed as argument.
+		Returns none if the request is timed out (5 seconds).
+		'''
+		def gettime_ntp(addr='time.nist.gov'):
+			# http://code.activestate.com/recipes/117211-simple-very-sntp-client/
+			TIME1970 = 2208988800      # Thanks to F.Lundh
+			client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+			data = '\x1b' + 47 * '\0'
+			try:
+				# Timing out the connection after 5 seconds, if no response received
+				client.settimeout(5.0)
+				client.sendto( data, (addr, 123))
+				data, address = client.recvfrom( 1024 )
+				if data:
+					epoch_time = struct.unpack( '!12I', data )[10]
+					epoch_time -= TIME1970
+					return epoch_time
+			except socket.timeout:
+				return None
+
+		if __name__ == "__main__":
+			# Iterates over every server in the list until it finds time from any one.
+			for server in server_list:
+				epoch_time = gettime_ntp(server)
+				if epoch_time is not None:
+					# SetSystemTime takes time as argument in UTC time. UTC time is obtained using utcfromtimestamp()
+					utcTime = datetime.datetime.utcfromtimestamp(epoch_time)
+					win32api.SetSystemTime(utcTime.year, utcTime.month, utcTime.weekday(), utcTime.day, utcTime.hour, utcTime.minute, utcTime.second, 0)
+					# Local time is obtained using fromtimestamp()
+					localTime = datetime.datetime.fromtimestamp(epoch_time)
+					print("Time updated to: " + localTime.strftime("%Y-%m-%d %H:%M") + " from " + server)
+					break
+				else:
+					print("Could not find time from " + server)
+
+	def test():
+		global test
+		test = "test1"
+
+
+
 	now = datetime.datetime.now()
 	timeString = now.strftime("%H:%M")
    
@@ -22,6 +78,8 @@ def simple():
    
 	cpu_usage = psutil.cpu_percent()
 	ram_usage = psutil.virtual_memory()[2]
+ 
+	
 	
 	
 	templateData = {
@@ -33,7 +91,8 @@ def simple():
 		'diskfree': diskfree,
 		'IP': IP,
 		'cpu_usage': cpu_usage,
-		'ram_usage': ram_usage
+		'ram_usage': ram_usage,
+		'test' : test
     }
 	return render_template('simple.html', **templateData)
 
